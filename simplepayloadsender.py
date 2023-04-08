d88e2ec0f4ab9b8a4e2d2847a6faa909c8b6a8bf
Technochip <100534634+Technochip@users.noreply.github.com>
2023-04-08 20:29:02 +0530

    Update simplepayloadsender.py

--- simplepayloadsender.py
+++ simplepayloadsender.py
@@ -2,7 +2,7 @@ import socket
 
 X = input("Enter the IP address: ")
 Y = int(input("Enter the port: "))
-Z = input("Enter the data to send: ")
+Z = input("Enter the payload to send: ")
 
 s = socket.socket()
 
@@ -15,8 +15,10 @@ except socket.error as e:
 
 try:
     s.send(Z.encode())
+    print("Sending successful!")
 except socket.error as e:
-    print("Error sending data:", e)
+    print("Sending failed:", e)
 finally:
     s.close()
+
 
