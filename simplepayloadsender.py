1ade3e4dc419afa76b35e94b2c2edb69b5177c7c
Technochip <100534634+Technochip@users.noreply.github.com>
2023-04-09 12:29:01 +0530

    Update simplepayloadsender.py

--- simplepayloadsender.py
+++ simplepayloadsender.py
@@ -2,23 +2,27 @@ import socket
 
 X = input("Enter the IP address: ")
 Y = int(input("Enter the port: "))
-Z = input("Enter the payload to send: ")
 
 s = socket.socket()
 
 try:
     s.connect((X, Y))
 except socket.error as e:
-    print("Error connecting to server:", e)
+    print("Error connecting to listner:", e)
     s.close()
     exit()
 
-try:
-    s.send(Z.encode())
-    print("Sending successful!")
-except socket.error as e:
-    print("Sending failed:", e)
-finally:
-    s.close()
+while True:
+    Z = input("Enter the payload to send (type 'exit' to quit): ")
+    if Z.lower() == 'exit':
+        break
+
+    try:
+        s.send(Z.encode())
+        print("Sending successful!")
+    except socket.error as e:
+        print("Sending failed:", e)
+
+s.close()
 
 
