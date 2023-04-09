6b84ee2af91937932bb4c402383b23978d699503
Technochip <100534634+Technochip@users.noreply.github.com>
2023-04-09 13:01:57 +0530

    Update simplelistener.py

--- simplelistener.py
+++ simplelistener.py
@@ -1,26 +1,22 @@
 import socket
 import subprocess
-import logging
 
-HOST = '127.0.0.1'
+HOST = ''
 PORT = int(input("Enter the port U want to listen on= "))
-
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
-logger = logging.getLogger()
 
 try:
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
     server_socket.listen(5)
-    logger.info(f'Listening on {HOST}:{PORT}...')
+    print(f'Listening on {HOST}:{PORT}...')
 except Exception as e:
-    logger.error(f'Failed to bind to {HOST}:{PORT} - {str(e)}')
+    print(f'Failed to bind to {HOST}:{PORT} - {str(e)}')
     exit(1)
 
 while True:
     try:
         client_socket, client_address = server_socket.accept()
-        logger.info(f'Accepted connection from {client_address[0]}:{client_address[1]}')
+        print(f'Accepted connection from {client_address[0]}:{client_address[1]}')
         message = 'Welcome to my Python listener!\n'
         client_socket.send(message.encode())
 
@@ -29,7 +25,7 @@ while True:
             if not data:
                 break
             command = data.decode().strip()
-            logger.debug(f'Received command: {command}')
+            print(f'Received command: {command}')
             try:
                 if command == 'exit':
                     break
@@ -46,16 +42,16 @@ while True:
                     if output == b'' and process.poll() is not None:
                         break
                     if output:
-                        logger.debug(f'Sending output: {output.decode()}')
+                        print(f'Sending output: {output.decode()}')
                         client_socket.send(output)
                 client_socket.send(prompt.encode())
             except Exception as e:
-                logger.error(f'Error executing command {command} - {str(e)}')
+                print(f'Error executing command {command} - {str(e)}')
                 client_socket.send(f'Error executing command {command}'.encode())
     except KeyboardInterrupt:
-        logger.info('Stopping listener due to keyboard interrupt')
+        print('Stopping listener due to keyboard interrupt')
         break
     except Exception as e:
-        logger.error(f'Error accepting connection - {str(e)}')
+        print(f'Error accepting connection - {str(e)}')
 
-server_socket.close()  
+server_socket.close()
